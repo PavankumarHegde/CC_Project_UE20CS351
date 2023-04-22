cd71a01c7f2e9d40744b233dbf15eeb27956d5c7
Pavankumar Hegde <51742316+PavankumarHegde@users.noreply.github.com>
2023-04-22 10:13:49 +0530

    Update config.php

--- CRUD/config.php
+++ CRUD/config.php
@@ -1,18 +1,23 @@
 <?php
 
-$databaseHost = 'localhost';
+$databaseHost = 'studentdb.c6nmgc0qv0u7.us-east-1.rds.amazonaws.com';
 $databaseName = 'test';
-$databaseUsername = 'root';
-$databasePassword = 'root';
+$databaseUsername = 'admin';
+$databasePassword = 'admin1234';
+
+//$databaseHost = 'localhost';
+//$databaseName = 'test';
+//$databaseUsername = 'username';
+//$databasePassword = 'Password123*';
 
 try {
-	// http://php.net/manual/en/pdo.connections.php
-	$dbConn = new PDO("mysql:host={$databaseHost};dbname={$databaseName}", $databaseUsername, $databasePassword);
-	
-	$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Setting Error Mode as Exception
-	// More on setAttribute: http://php.net/manual/en/pdo.setattribute.php
+        // http://php.net/manual/en/pdo.connections.php
+        $dbConn = new PDO("mysql:host={$databaseHost};dbname={$databaseName}", $databaseUsername, $databasePassword);
+
+        $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Setting Error Mode as Exception
+        // More on setAttribute: http://php.net/manual/en/pdo.setattribute.php
 } catch(PDOException $e) {
-	echo $e->getMessage();
+        echo $e->getMessage();
 }
  
 ?>
